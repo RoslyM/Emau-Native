847a237dcc0750e50c5e5b601ae0cfa11a6b0437
Rosly <rosly.mokambo@ginov.io>
2024-03-16 18:17:16 +0100

    Version Stable

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import Orientation from 'react-native-orientation-locker';
 import Navigation from './navigation';
-import {Provider} from "react-redux"
-import {store} from './slice/redux'
+import { Provider } from 'react-redux';
+import { store } from './slice/redux';
 
 export default function App() {
+  useEffect(() => {
+    // Verrouiller l'orientation en mode paysage
+    Orientation.lockToLandscape();
+    
+    // Déverrouiller l'orientation lorsqu'elle n'est plus nécessaire
+    return () => {
+      Orientation.unlockAllOrientations();
+    };
+  }, []);
+
   return (
     <Provider store={store}>
-        <Navigation/>
+      <Navigation/>
     </Provider>
   );
 }
-
